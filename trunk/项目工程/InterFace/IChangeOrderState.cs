5f34140b86d0d674f6fc0575acece4adfadd0bd6
likailin1986 <likailin1986@ca434122-b41d-11de-8e13-c326f17d7b4c>
2009-12-16 11:05:14 +0000

    修改IChangeOrderState接口 添加OrderInfoBLL

--- trunk/项目工程/InterFace/IChangeOrderState.cs
+++ trunk/项目工程/InterFace/IChangeOrderState.cs
@@ -2,10 +2,12 @@
 ////日  期：2009-11-25
 ////功  能：修改订单状态接口
 
+using Entity;
+
 namespace InterFace
 {
     public interface IChangeOrderState
     {
-        bool ChangeOrderState();
+        bool ChangeOrderState(OrderInfo orderInfo);
     }
 }
