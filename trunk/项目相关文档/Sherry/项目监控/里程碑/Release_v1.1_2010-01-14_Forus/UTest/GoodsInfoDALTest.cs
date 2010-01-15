a385d0fb1558995595efe0720f0b52df8fc8e4b2
kimcherwoo@gmail.com <kimcherwoo@gmail.com@ca434122-b41d-11de-8e13-c326f17d7b4c>
2010-01-15 06:44:39 +0000

    kim

---
+++ trunk/项目相关文档/Sherry/项目监控/里程碑/Release_v1.1_2010-01-14_Forus/UTest/GoodsInfoDALTest.cs
@@ -0,0 +1,176 @@
+﻿using DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entity;
+using System;
+using System.Data;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///这是 GoodsInfoDALTest 的测试类，旨在
+    ///包含所有 GoodsInfoDALTest 单元测试
+    ///</summary>
+    [TestClass()]
+    public class GoodsInfoDALTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试属性
+        // 
+        //编写测试时，还可使用以下属性:
+        //
+        //使用 ClassInitialize 在运行类中的第一个测试前先运行代码
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //使用 ClassCleanup 在运行完类中的所有测试后再运行代码
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //使用 TestInitialize 在运行每个测试前先运行代码
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //使用 TestCleanup 在运行完每个测试后运行代码
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///LatestGoods 的测试
+        ///</summary>
+        [TestMethod()]
+        public void LatestGoodsTest()
+        {
+            int num = 5; // TODO: 初始化为适当的值
+            bool orderByVolume = false; // TODO: 初始化为适当的值
+            DataTable actual;
+            actual = GoodsInfoDAL.LatestGoods(num, orderByVolume);
+            Assert.IsNotNull(actual);
+            
+        }
+
+        /// <summary>
+        ///GoodsDetail 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GoodsDetailTest()
+        {
+            int goodsID = 1; // TODO: 初始化为适当的值
+            GoodsInfo actual;
+            actual = GoodsInfoDAL.GoodsDetail(goodsID);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///GetCategory 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GetCategoryTest()
+        {
+            DataTable category = null; // TODO: 初始化为适当的值
+            DataTable categoryExpected = null; // TODO: 初始化为适当的值
+            bool expected = false; // TODO: 初始化为适当的值
+            bool actual;
+            actual = GoodsInfoDAL.GetCategory(ref category);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///FindSpecialGoods 的测试
+        ///</summary>
+        [TestMethod()]
+        public void FindSpecialGoodsTest()
+        {
+            DataTable expected = null; // TODO: 初始化为适当的值
+            DataTable actual;
+            actual = GoodsInfoDAL.FindSpecialGoods();
+            Assert.IsNotNull(actual);
+                
+        }
+
+        /// <summary>
+        ///FindGoods 的测试
+        ///</summary>
+        [TestMethod()]
+        public void FindGoodsTest()
+        {
+            string goodsName = "test"; // TODO: 初始化为适当的值
+            string goodsNum = "1"; // TODO: 初始化为适当的值
+            int goodsCategory = 1; // TODO: 初始化为适当的值
+            DateTime timeFrom = new DateTime(2009,1,1); // TODO: 初始化为适当的值
+            DateTime timeTo = new DateTime(2009,12,30); // TODO: 初始化为适当的值
+            DataTable actual;
+            actual = GoodsInfoDAL.FindGoods(goodsName, goodsNum, goodsCategory, timeFrom, timeTo);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///DeleteGoods 的测试
+        ///</summary>
+        [TestMethod()]
+        public void DeleteGoodsTest()
+        {
+            int goodsID = 99999; // TODO: 初始化为适当的值
+            bool expected = false; // TODO: 初始化为适当的值
+            bool actual;
+            actual = GoodsInfoDAL.DeleteGoods(goodsID);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///ChangeGoods 的测试
+        ///</summary>
+        [TestMethod()]
+        public void ChangeGoodsTest()
+        {
+            int goodsID = 0; // TODO: 初始化为适当的值
+            GoodsInfo goodsInfo = new GoodsInfo(); // TODO: 初始化为适当的值
+            goodsInfo.goodsAddTime = DateTime.Now;
+            goodsInfo.goodsAvailable = true;
+            goodsInfo.goodsCategory = 1;
+            goodsInfo.goodsDescribe="test again";
+            goodsInfo.goodsName="test again";
+            goodsInfo.goodsNum="100";
+            goodsInfo.goodsPrice=100;
+            goodsInfo.goodsSpecialOffer=1;
+            goodsInfo.goodsStorage=100;
+            goodsInfo.goodsValidity=new DateTime(2012,12,26);
+            bool expected = false; // TODO: 初始化为适当的值
+            bool actual;
+            actual = GoodsInfoDAL.ChangeGoods(goodsID, goodsInfo);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ trunk/项目相关文档/Sherry/项目监控/里程碑/Release_v1.1_2010-01-14_Forus/UTest/MarketDecisionDALTest.cs
@@ -0,0 +1,182 @@
+﻿using DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///这是 MarketDecisionDALTest 的测试类，旨在
+    ///包含所有 MarketDecisionDALTest 单元测试
+    ///</summary>
+    [TestClass()]
+    public class MarketDecisionDALTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试属性
+        // 
+        //编写测试时，还可使用以下属性:
+        //
+        //使用 ClassInitialize 在运行类中的第一个测试前先运行代码
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //使用 ClassCleanup 在运行完类中的所有测试后再运行代码
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //使用 TestInitialize 在运行每个测试前先运行代码
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //使用 TestCleanup 在运行完每个测试后运行代码
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///MemberShopSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void MemberShopSalesAmountTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            int sAge = 0; // TODO: 初始化为适当的值
+            int eAge = 0; // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009,1,1); // TODO: 初始化为适当的值
+            DateTime eDate = DateTime.Now; // TODO: 初始化为适当的值            
+            int actual;
+            actual = target.MemberShopSalesAmount(sAge, eAge, sDate, eDate);
+            Assert.IsNotNull(actual);
+                        
+        }
+
+        /// <summary>
+        ///MemberPhoneSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void MemberPhoneSalesAmountTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            int sAge = 0; // TODO: 初始化为适当的值
+            int eAge = 100; // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009,1,1); // TODO: 初始化为适当的值
+            DateTime eDate =DateTime.Now; // TODO: 初始化为适当的值
+
+            int actual;
+            actual = target.MemberPhoneSalesAmount(sAge, eAge, sDate, eDate);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///MemberOnlineSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void MemberOnlineSalesAmountTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            int sAge = 0; // TODO: 初始化为适当的值
+            int eAge = 100; // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009, 1, 1); // TODO: 初始化为适当的值
+            DateTime eDate = DateTime.Now; // TODO: 初始化为适当的值
+
+            int actual;
+            actual = target.MemberOnlineSalesAmount(sAge, eAge, sDate, eDate);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///GoodsShopSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GoodsShopSalesAmountTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009,1,1); // TODO: 初始化为适当的值
+            DateTime eDate = DateTime.Now; // TODO: 初始化为适当的值
+            string categoryName = "面部护理"; // TODO: 初始化为适当的值
+            
+            int actual;
+            actual = target.GoodsShopSalesAmount(sDate, eDate, categoryName);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///GoodsPhoneSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GoodsPhoneSalesAmountTest()
+        {
+            
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009, 1, 1); // TODO: 初始化为适当的值
+            DateTime eDate = DateTime.Now; // TODO: 初始化为适当的值
+            string categoryName = "面部护理"; // TODO: 初始化为适当的值
+
+            int actual;
+            actual = target.GoodsPhoneSalesAmount(sDate, eDate, categoryName);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///GoodsOnlineSalesAmount 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GoodsOnlineSalesAmountTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            DateTime sDate = new DateTime(2009, 1, 1); // TODO: 初始化为适当的值
+            DateTime eDate = DateTime.Now; // TODO: 初始化为适当的值
+            string categoryName = "面部护理"; // TODO: 初始化为适当的值
+
+            int actual;
+            actual = target.GoodsOnlineSalesAmount(sDate, eDate, categoryName);
+            Assert.IsNotNull(actual);
+        }
+
+        /// <summary>
+        ///CountMembersAgeGroup 的测试
+        ///</summary>
+        [TestMethod()]
+        public void CountMembersAgeGroupTest()
+        {
+            MarketDecisionDAL target = new MarketDecisionDAL(); // TODO: 初始化为适当的值
+            int sAge = 1; // TODO: 初始化为适当的值
+            int eAge = 100; // TODO: 初始化为适当的值
+
+            int actual;
+            actual = target.CountMembersAgeGroup(sAge, eAge);
+            Assert.IsNotNull(actual);
+        }
+    }
+}
